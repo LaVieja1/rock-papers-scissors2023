7a8cc1eb122954e85bd96a2f0137887ea79bfed0
santicano <canosantiago404@gmail.com>
2023-03-28 17:57:24 -0300

    Añadida la funcion de conseguir la eleccion de la computadora. Aleatoriamente devuelve piedra papel o tijera

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+
+    let choice = '';
+    const option = Math.floor((Math.random() * 3) + 1);
+    
+    if (option === 1) {
+        choice = "Piedra";
+    } else if (option === 2) {
+        choice = "Papel";
+    } else {
+        choice = "Tijera";
+    }
+
+    return choice;
+}
